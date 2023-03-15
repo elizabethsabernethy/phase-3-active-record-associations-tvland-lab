f01be6c314aa86c642c04d8f541cc888b90571df
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-15 19:39:23 -0400

    commit tree 73b22241695738a02fdf7308c70a16876769fae8

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles=[]
+    character_name=""
+    show_name=""
+
+    self.characters.map do |character|
+    character_name = "#{character.name}"
+    end
+    self.shows.map do |show|
+    show_name = "#{show.name}"
+    end
+
+    roles << "#{character_name} - #{show_name}"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+  def actors_list
+    list=""
+    first=""
+    last=""
+    self.actors.map do |actor|
+        first="#{actor.first_name}"
+        last="#{actor.last_name}"
+    end
+    list = "#{first} #{last}"
+  end
 end
